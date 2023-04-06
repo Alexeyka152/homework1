0324ac4337cb62592e699b9076f5e8956c9f5720
Алексей Шредингера <ovsyanykov@gmail.com>
2023-04-06 19:44:58 +0300

    Ну все, точно финал

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,53 +1,36 @@
 import java.io.File
 import java.io.FileWriter
 
-fun main()
-{
-    //Даа я знаю что я ужасный программист, пожалуйста, не бейте сильно
-    println("Если вы хотите считать текст из файла, поместите его в одну папку с Main.kt и введите название файла в формате name.txt, либо укажите путь к файлу в формате C:\\Users\\Documents\\name.txt")
+fun main() {
+    // Да, я знаю, что я ужасный программист. Пожалуйста, не бейте сильно.
+    println("Если вы хотите считать текст из файла, поместите его в одну папку с Main.kt и введите название файла в формате name.txt")
     println("Если вы не хотите считывать текст из файла, можете просто ввести его в консоль.")
-    val input: String = readLine() ?: "";
-    println();
-    if (input.split("\\").size > 1) //если есть хотя бы один слеш, то есть мнение, что это путь к файлу
-    {
-        getStatistic(File(input).readText(), input);
-        println("Аналогичная статистика добавлена в исходный .txt файл");
-    }
-    else if (input.takeLast(4) == ".txt") //если слешей нет, но предложение заканчивается на .txt, то скорее всего это название файла)
-    {
-        getStatistic(File("src\\main\\kotlin\\$input").readText(), "src\\main\\kotlin\\$input");
-        println("Аналогичная статистика добавлена в исходный .txt файл");
-    }
-    else //если просто введен текст
-    {
-        getStatistic(input);
-    }
+    val input: String = readLine() ?: ""
+    println()
+    getStatistic(input)
 }
 
-fun getStatistic(text: String, filePath: String = " ") {
-    val sentences = text.trimEnd('.', '!', '?').split(".", "!", "?");
-    val numberOfWords = sentences.map { it.trimStart().trimEnd().split(Regex("\\s+")).size }
-
-    println("Предложения и количество слов в них:");
-
-    val fileWriter = try
-    {
-        FileWriter(File(filePath), true)
-    }
-    catch (e: Exception)
-    {
+fun getStatistic(text: String) {
+    val fileWriter = try {
+        FileWriter("src\\main\\kotlin\\$text", true)
+    } catch (e: Exception) {
         null
     }
-
-    for (i in 0 until sentences.size)
-    {
-        println("Количество слов: ${numberOfWords[i]}. ${sentences[i].trimStart().trimEnd()}");
-        if (filePath != " ")
-        {
-            //File(filePath).writeText("Количество слов: ${numberOfWords[i]}. ${sentences[i].trimStart().trimEnd()}");
-            fileWriter?.write("\n");
-            fileWriter?.write("Количество слов: ${numberOfWords[i]}. ${sentences[i].trimStart().trimEnd()}");
-        }
+    val finalText = try{
+        File("src\\main\\kotlin\\$text").readText()
+    } catch (e: Exception) {
+        text
     }
-    fileWriter?.close();
+    val sentences = finalText.trimEnd('.', '!', '?').split(".", "!", "?")
+    val numberOfWords = sentences.map { it.trimStart().trimEnd().split(Regex("\\s+")).size }
+    println("Предложения и количество слов в них:")
+    for (i in 0 until sentences.size) {
+        println("Количество слов: ${numberOfWords[i]}. ${sentences[i].trimStart().trimEnd()}")
+        fileWriter?.write("\n")
+        fileWriter?.write("Количество слов: ${numberOfWords[i]}. ${sentences[i].trimStart().trimEnd()}")
+    }
+    if (fileWriter != null){
+        println("Аналогичная статистика записана в исходный .txt файл")
+    }
+    fileWriter?.close()
 }
